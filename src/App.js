2afeb17668ba6f2938263d5b4d0d23e6f38b495a
Mateus Back <m.back@nextage.com.br>
2024-07-23 22:31:38 -0300

    chore: Add react-router-dom package and update App.js for routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/home';
+import Login from './pages/login/login';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Header />
+   <BrowserRouter>
+   <Routes>
+      <Route path="/" Component={Home} />
+      <Route path="/login" Component={Login} />
+      </Routes>
+    </BrowserRouter>
+    <Footer />
+   </>
   );
 }
 
